14f9deb764ea6b92b8c98da1e4a3d9eac9dd1986
Bit Mango <291897687+bit-mango@users.noreply.github.com>
2026-06-16 21:02:21 -0700

    Add randdp function to generate psuedo random numbers for NPB kernels.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,77 @@
+use std::time::Instant;
 // randdp, timing, verification, class sizes
+
+const A: u128 = 5_u128.pow(13);
+const B: u128 = 1_u128 << 46;
+// Want to generate n uniform psuedo random numbers.
+// a = 5^13
+// x_0 = s, a specified initial seed. Where 0 < s < 2^46
+// Generate the integers x_k for 1 <= k <= n using the following:
+// x_k+1 = a*x_k % 2^46
+// Then return r_k = 2^(-46)*x_k
+// Thus 0 < r_k < 1
+pub fn randdp(seed: u128, n: usize) -> Vec<f64> {
+    let mut r_k = Vec::with_capacity(n);
+    let mut prev_x_k = seed;
+
+    for _ in 0..n {
+        let x_k = (A * prev_x_k) % B;
+        r_k.push(2.0_f64.powi(-46) * x_k as f64);
+        prev_x_k = x_k;
+    }
+
+    r_k
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validate_rngddp_range() {
+        // All random numbers, r, should follow this rule 0 < r < 1.
+        let npb_seed = 271828182845904523;
+        let r = randdp(npb_seed, 1_000);
+        for elem in r.iter() {
+            assert!(
+                *elem > 0.0 && *elem < 1.0,
+                "Random numbers should fall between 0 and 1, got {:}",
+                elem
+            );
+        }
+    }
+
+    #[test]
+    fn validate_rngddp_distribution() {
+        // Should be evenly distributed such that n/10 numbers fall between 0.0 -> 0.1 and so on.
+        let npb_seed = 271828182845904523;
+        let r = randdp(npb_seed, 1_000);
+        let mut ranges = vec![];
+        let mut lower = 0.0;
+        let mut upper = 0.1;
+        for _ in 0..10 {
+            ranges.push((lower, upper));
+            lower = upper;
+            upper += 0.1;
+        }
+
+        let mut count = vec![0_u32; 10];
+        for elem in r.iter() {
+            for j in 0..10 {
+                if ranges[j].0 < *elem && *elem <= ranges[j].1 {
+                    count[j] = count[j] + 1;
+                    break;
+                }
+            }
+        }
+
+        for i in 0..10 {
+            assert!(
+                count[i] > 90 && count[i] < 110,
+                "Expected equal distribution, but got {}, in {}",
+                count[i],
+                i
+            );
+        }
+    }
+}
